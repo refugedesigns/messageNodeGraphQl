8dd4be148db056e3003fb5fef534273ec7afc6b0
Erasmus Antwi <refugedesigns17@gmail.com>
2021-07-09 03:52:24 +0000

    added socket.io

--- app.js
+++ app.js
@@ -58,7 +58,11 @@ mongoose
     `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.uv3ti.mongodb.net/messages?retryWrites=true&w=majority`
   )
   .then((res) => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require("./socket").init(server)
+    io.on("connection", socket => {
+      console.log("Client connected")
+    })
   })
   .catch((err) => {
     console.log(err);
--- controllers/feed.js
+++ controllers/feed.js
@@ -2,7 +2,8 @@ const path = require("path");
 const fs = require("fs");
 const { validationResult } = require("express-validator");
 const Post = require("../models/post");
-const User = require("../models/user")
+const User = require("../models/user");
+const io = require("../socket");
 
 const clearImage = (filePath) => {
   filePath = path.join(__dirname, "..", filePath);
@@ -18,7 +19,9 @@ exports.getPosts = (req, res, next) => {
     .then((numOfPosts) => {
       totalPosts = numOfPosts;
       return Post.find()
-        .skip(currentPage - 1)
+        .populate("creator")
+        .sort({ createdAt: -1 })
+        .skip((currentPage - 1) * perPage)
         .limit(perPage);
     })
     .then((posts) => {
@@ -33,20 +36,22 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.getUserStatus = (req, res, next) => {
-  User.findById(req.userId).then(user => {
-    if(!user) {
-      const error = new Error("No user found.")
-      error.statusCode = 404
-      throw error
-    }
-    res.status(200).json({status: user.status})
-  }).catch(err => {
-    if (!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
-  })
-}
+  User.findById(req.userId)
+    .then((user) => {
+      if (!user) {
+        const error = new Error("No user found.");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ status: user.status });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
@@ -70,22 +75,31 @@ exports.createPost = (req, res, next) => {
     title: title,
     content: content,
     imageUrl: image.path,
-    creator: req.userId
+    creator: req.userId,
   });
   post
     .save()
     .then((result) => {
-      postData = result
-      return User.findById(req.userId)
-    }).then(user => {
-      creator = user
-      user.posts.push(post)
-      return user.save()
-    }).then(response => {
+      postData = result;
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
+    .then((response) => {
+      io.getIO().emit("posts", {
+        action: "create",
+        post: {
+          ...post._doc,
+          creator: { _id: req.userId, name: creator.name },
+        },
+      });
       res.status(201).json({
         message: "Post created successfully!",
         post: post,
-        creator: {_id: creator._id, name: creator.name}
+        creator: { _id: creator._id, name: creator.name },
       });
     })
     .catch((err) => {
@@ -142,16 +156,17 @@ exports.updatePost = (req, res, next) => {
   }
 
   Post.findById(postId)
+    .populate("creator")
     .then((post) => {
       if (!post) {
         const error = new Error("Could not find post.");
         error.statusCode = 404;
         throw error;
       }
-      if(post.creator._id.toString() !== req.userId) {
-        const error = new Error("Not authorized.")
+      if (post.creator._id.toString() !== req.userId) {
+        const error = new Error("Not authorized.");
         error.statusCode = 403;
-        throw error
+        throw error;
       }
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
@@ -162,6 +177,7 @@ exports.updatePost = (req, res, next) => {
       return post.save();
     })
     .then((result) => {
+      io.getIO().emit("posts", { action: "update", post: result });
       res.status(200).json({ message: "Post updated!", post: result });
     })
     .catch((err) => {
@@ -173,26 +189,34 @@ exports.updatePost = (req, res, next) => {
 };
 
 exports.updateUserStatus = (req, res, next) => {
-  const updatedStatus = req.body.status
+  const updatedStatus = req.body.status;
   let loadedUser;
-  User.findById(req.userId).then(user => {
-    if(!user) {
-      const error = new Error("No user found.")
-      error.statusCode = 404
-      throw error
-    }
-    loadedUser = user
-    user.status = updatedStatus 
-    return user.save()
-  }).then(result => {
-    res.status(201).json({message: "Users status updated successfully.", status: loadedUser.status})
-  }).catch(err => {
-    if (!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
-  })
-}
+  User.findById(req.userId)
+    .then((user) => {
+      if (!user) {
+        const error = new Error("No user found.");
+        error.statusCode = 404;
+        throw error;
+      }
+      loadedUser = user;
+      user.status = updatedStatus;
+      return user.save();
+    })
+    .then((result) => {
+      res
+        .status(201)
+        .json({
+          message: "Users status updated successfully.",
+          status: loadedUser.status,
+        });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
 
 exports.deletePost = (req, res, next) => {
   const postId = req.params.postId;
@@ -214,11 +238,14 @@ exports.deletePost = (req, res, next) => {
       return Post.findByIdAndRemove(postId);
     })
     .then((result) => {
-      return User.findById(req.userId)
-    }).then(user => {
-      user.posts.pull(postId)
-      return user.save()
-    }).then(result => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then((result) => {
+      io.getIO().emit("posts", {action: "delete", post: postId})
       res.status(200).json({ message: "Post deleted" });
     })
     .catch((err) => {
